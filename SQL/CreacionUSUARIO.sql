024ac7a0e54ad8bb8c89d7c3774d00b7f6d78b6f
Rafael Morales Moreno <92787464+Rafmormor3@users.noreply.github.com>
2022-12-15 08:59:38 +0100

    Delete SQL directory

--- SQL/CreacionUSUARIO.sql
+++
@@ -1,3 +0,0 @@
-alter session set "_oracle_script"=true;  
-create user jugadores identified by jugadores;
-GRANT CONNECT, RESOURCE, DBA TO jugadores;
--- SQL/Script/ScriptPez.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE PEZ
-(
-	nombreCientifico VARCHAR2(50),
-	nombreComun VARCHAR2(50),
-	CONSTRAINT PK_PEZ PRIMARY KEY (nombreCientifico)
-);
