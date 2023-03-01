6900db8f9064213d15dd2d5027ef2a38df2543c3
rafamorales <rafamorales@gmail.com>
2023-03-01 17:06:51 +0100

    Actualizacion

--- Oracle/Consultas/consultaPelicula.sql
+++ Oracle/Consultas/consultaPelicula.sql
@@ -5,8 +5,8 @@ FROM CINE c ;
 --2. Obtener las películas con un presupuesto mayor de 2000 o cuya duración sea superior a 100.
 SELECT P.TITULO_P 
 FROM PELICULA p 
-WHERE  P.PRESUPUESTO > 2000
- AND P.DURACION >100;
+WHERE  (P.PRESUPUESTO > 2000)
+ OR (P.DURACION >100);
 
 --3. Obtener las películas cuyo título (da igual el original o el español) contenga la cadena la sin
 --   importar que esté en mayúsculas o minúsculas.
@@ -29,9 +29,8 @@ WHERE P.CIP = P2.CIP
  AND EXTRACT(MONTH FROM P2.FECHA_ESTRENO)=9;
 
 --6. Obtener las diferentes tareas que ha desempeñado alguna persona alguna vez.
-SELECT T.TAREA 
-FROM TRABAJO T, PERSONAJE p  
-WHERE T.NOMBRE_PERSONA  = P.NOMBRE_PERSONA  ; 
+SELECT DISTINCT T.TAREA 
+FROM TRABAJO T; 
 
 --7. Obtener el numero de sala y el aforo de todas las salas de los cines que terminen en vocal.
 SELECT S.SALA , S.AFORO , S.CINE 
@@ -59,17 +58,20 @@ WHERE P.CIP = P2.CIP
 ORDER BY (P2.RECAUDACION-P.PRESUPUESTO) DESC;
 
 --10. Obtener el nombre de los actores hombres que participen en la película Viaje al centro de la tierra.
-SELECT T.NOMBRE_PERSONA 
-FROM TRABAJO t , PELICULA p 
+SELECT T.NOMBRE_PERSONA
+FROM TRABAJO t , PELICULA p , PERSONAJE p2 
 WHERE T.CIP = P.CIP 
+ AND P2.NOMBRE_PERSONA = T.NOMBRE_PERSONA 
+ AND P2.SEXO_PERSONA LIKE 'H'
+ AND UPPER(T.TAREA) LIKE '%ACTOR%'
  AND P.TITULO_P LIKE 'Viaje al centro de la tierra';
 
 --11. Obtener el nombre del cine y el número de películas diferentes estrenadas por cada cine ordenadas por el número de películas 
 --    ordenadas de mayor a menor.
-SELECT P.CINE , COUNT(DISTINCT P.FECHA_ESTRENO) AS NUM_ESTRENOS
+SELECT P.CINE , COUNT(DISTINCT P.CIP) AS NUM_ESTRENOS
 FROM PROYECCION p 
 GROUP BY P.CINE
-ORDER BY NUM_ESTRENOS ;
+ORDER BY NUM_ESTRENOS DESC;
 
 --12. Obtener el nombre y nacionalidad de las personas que hayan trabajado en alguna película de diferente nacionalidad a la suya.
 SELECT P.NOMBRE_PERSONA , P.NACIONALIDAD_PERSONA
@@ -103,6 +105,7 @@ SELECT P.TITULO_P , P.ANO_PRODUCCION , P.PRESUPUESTO , SUM(P2.RECAUDACION) AS RE
 FROM PELICULA p , PROYECCION p2, SALA s , CINE c  
 WHERE P.CIP = P2.CIP
  AND P2.CINE  = S.CINE 
+ AND P2.SALA = S.SALA 
  AND S.CINE = C.CINE 
  AND C.CIUDAD_CINE  LIKE 'Cordoba'
 GROUP BY P.TITULO_P , P.ANO_PRODUCCION , P.PRESUPUESTO;
@@ -119,4 +122,5 @@ FROM PERSONAJE p , TRABAJO t
 WHERE P.NOMBRE_PERSONA = T.NOMBRE_PERSONA 
 GROUP BY P.NOMBRE_PERSONA 
 HAVING COUNT(T.NOMBRE_PERSONA)>2;
+
 
